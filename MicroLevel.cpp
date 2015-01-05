da06d186a46870a0ce34e35363f41319483f2c23
kvasilakes <my427v8@hotmail.com>
2015-01-04 18:31:20 -0600

    Usubg templated ring buffer Added I2c driver

--- MicroLevel.cpp
+++ MicroLevel.cpp
@@ -9,11 +9,15 @@
 #include "CSerial.h"
 #include "CTimer.h"
 #include "CBlink.h"
+#include "i2cmaster.h"
+#include <avr/io.h>
+
 
 uint16_t a = 0;
 uint8_t b = 0;
 uint8_t txbuf[100];
 uint8_t rxbuf[100];
+
 
 int main(void)
 {
@@ -22,14 +26,66 @@ int main(void)
 	
 	CTimer::Init();
 	uint32_t start = CTimer::GetTick();
+	uint32_t LEDS = CTimer::GetTick();
+	bool tock = false;
+	
+	//Turn on 5v, PA2
+	DDRA |= _BV(DDA2);
+	PORTA |= _BV(PA2);
+	
+	i2c_init();                             // initialize I2C library
+
+	// LED controller
+	//if(i2c_start_wait(0x40))     // set device address and write mode
+	//{
+		//Serial << "i2c start\r\n"
+		//i2c_write(0x0c);                        // write address = 0x0c
+		//i2c_write(0x55);                        // write value 0b01010101
+		//i2c_stop();                             // set stop conditon = release bus
+	//}
+	//
+	//i2c_start_wait(0x40);     // set device address and write mode
+	//i2c_write(0x0D);                        // write address = 0x0c
+	//i2c_write(0x55);                        // write value 0b01010101
+	//i2c_stop();                             // set stop conditon = release bus
 
     while(1)
     {
 		if(CTimer::IsTimedOut(100, start))
 		{
-			Serial <<  a++ << ", " << b-- << ", Hello World 3!\r\n";
+			Serial <<  a++ << ", " << b-- << ", Hello World y!\r\n";
 			
 			start = CTimer::GetTick();
+		}
+		
+		if(CTimer::IsTimedOut(1000, LEDS))
+		{
+			uint8_t i = i2c_start_wait(0x40);
+			
+			if(i == 1)     // set device address and write mode
+			{
+				Serial << "i2c start\r\n";
+				i2c_write(0x0c);                        // write address = 0x0c
+				i2c_write(0x55);                        // write value 0b01010101
+				i2c_stop();                             // set stop conditon = release bus
+				
+				LEDS = CTimer::GetTick();
+			}
+			else
+			{
+				Serial << "i2c Failed\r\n";
+				
+				LEDS = CTimer::GetTick();
+				
+				//i2c_start_wait(0x40+I2C_WRITE);     // set device address and write mode
+				//i2c_write(0x0c);                        // write address = 0x0c
+				//i2c_write(0x55);                        // write value 0b01010101
+				//i2c_stop();
+				//
+				//LEDS = CTimer::GetTick();
+			}
+			
+			tock = ~tock;
 		}
     }
 }
---
+++ circularbuffer.h
@@ -0,0 +1,127 @@
+/* *****************************************************************************
+*  A templated circular buffer. 
+*  ***************************************************************************** */
+
+#pragma once
+#define __PROG_TYPES_COMPAT__
+#include <stdint.h>
+#include <stddef.h>
+
+template <class T, uint16_t MAX_ENTRIES> class CircularBuffer
+{
+  T Buffer[MAX_ENTRIES];
+
+  // Head is the location of the last value added to the
+  // circular buffer. 
+  int In;
+  int Out;
+
+  // Track the number of entries stored in the buffer. 
+  // Never greater than MAX_ENTRIES;
+  int Size;
+
+public:
+  CircularBuffer():
+  In(0),
+  Out(0),
+  Size(0)
+    /* Initializes an empty buffer.  */
+  {
+  }
+
+  uint8_t ElementSize()
+  {
+    return sizeof(T);
+  }
+
+  bool IsEmpty()
+    /* Returns true if no data has been stored in the buffer, 
+    and false if at least one value has been stored. */
+  {
+    return Size == 0;
+  }
+
+    /* Add a new value to the end of the buffer. If the
+    buffer has exceed its intrinsic capacity, this will
+    overwrite old entries. */
+  bool Put(const T c)
+  {
+	uint16_t in = In+1;
+	
+    if (in+1 >= MAX_ENTRIES -1)
+	{
+      in = 0;
+	}
+	
+	if(in != Out)
+	{
+		Buffer[In] = c;
+		
+		In = in;
+		return true;
+	}
+	
+	return false;
+  }
+  
+  //allows adding after buffer is full, 
+  //Will overwrite old data
+  void Add(T c)
+  {
+	  Buffer[In++] = c;
+
+	  //handle wrap
+	  if( In >= MAX_ENTRIES-1)
+	  In = 0;
+  }
+
+//Get a byte from the buffer
+//Removes byte
+bool Get(T &c)
+{
+	//buffer is empty?
+	if(In != Out)
+	{
+		//not empty yet
+		c = Buffer[Out];
+		
+		Out++;
+		
+		//handle wrap
+		if(Out > MAX_ENTRIES-1)
+		{
+			Out = 0;
+		}
+		
+		return true;
+	}
+	
+	return false;
+}
+
+/* Empties the circular buffer. The old data is 
+    still present, but iterators and count will 
+    ignore it. */
+  void Clear()
+  {
+    Size = 0;
+	In = 0;
+	Out = 0;
+  }
+
+/* Returns the number of entries that are stored
+in the circular buffer. */
+  int CountStored() const
+  {
+    return Size;
+  }
+
+/* Returns the maximum number of values that can
+be stored in the circular buffer. */
+  int MaxSize() const
+  {
+    return MAX_ENTRIES;
+  }
+
+};
+
