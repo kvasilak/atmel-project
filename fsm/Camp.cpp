7cbd2144b473295bb29b1969e36b4f5e7e6fc5ff
keith vasilakes <my427v8@hotmail.com>
2015-08-22 22:46:03 -0500

    Camp mode Alpha build

--- fsm/Camp.cpp
+++ fsm/Camp.cpp
@@ -12,8 +12,8 @@
 #include "..\CSerial.h"
 #include "nvm.h"
 
-static const int16_t pitchtol = 64;
-static const int16_t rolltol = 64;
+static const int16_t pitchtol = 10;
+static const int16_t rolltol = 10;
 
 FsmCamp::FsmCamp(CController& SMManager) :
 CState(SMManager, eStates::STATE_CAMP),
@@ -102,20 +102,6 @@ void FsmCamp::OnExit()
 
 //Accel should be orientated such that X is at 0 G and Z is at zero G
 //This would be with the corner dot in the upper left
-// 
-// 
-// pitch   roll       left  right
-// ok       ok         hold   hold
-// ok       leftlow    fill   dump
-// ok       rightlow   dump   fill
-// ok       lefthigh   dump   fill
-// ok       righthigh  fill   dump
-// low      ok         fill   fill
-// low      leftlow    fill   hold
-// low      rightlow   hold   fill
-// high     ok         dump   dump
-// high     lefthigh   dump   hold
-// high     righthigh  hold   dump
 
 void FsmCamp::LevelIt()
 {
@@ -132,11 +118,15 @@ void FsmCamp::LevelIt()
   //pitch up if( Z > pitchcal + pitchtol )
  {
 	 CSerial::is() << "pitch up\n";
+	 Cio::is().Left(eValveStates::Dump);
+	 Cio::is().Right(eValveStates::Dump);
  }
  //pitch down
  else if( Z < pitchcal - pitchtol )
  {
 	 CSerial::is() << "pitch down\n";
+	 Cio::is().Left(eValveStates::Fill);
+	 Cio::is().Right(eValveStates::Fill);
  }
  //pitch within tolerance
  else
@@ -145,111 +135,25 @@ void FsmCamp::LevelIt()
 	 if(X > rollcal + rolltol)
 	 {
 		 CSerial::is() << "roll up\n";
+		 Cio::is().Left(eValveStates::Fill);
+		 Cio::is().Right(eValveStates::Dump);
 	 }
 	 //Roll down (Right Down)
 	 else if(X < rollcal - rolltol)
 	 {
 		 CSerial::is() << "roll down\n";
+		 Cio::is().Left(eValveStates::Dump);
+		 Cio::is().Right(eValveStates::Fill);
 	 }
 	 //Roll within tolerance
 	 else
 	 {
 		 CSerial::is() << "pitch and roll within tolerance\n";
+		 Cio::is().Left(eValveStates::Hold);
+		 Cio::is().Right(eValveStates::Hold);
+		 
+		 LevelComplete = true;
 	 }
- }
-
- 	//Pitch high
-// 	if( pitch > Z + pitchtol )// 	{ 		// pitch   roll       left  right 		// high     lefthigh   dump   hold
-//  		if(roll > X + rolltol)
-//  		{
-//  			Cio::is().Left(eValveStates::Dump);
-//  			Cio::is().Right(eValveStates::Hold);
-//  		}
-// 
-// 
- 		// pitch   roll       left  right
-//  		// high     righthigh  hold   dump
-//  		else if(roll < X - rolltol)
-//  		{
-//  			Cio::is().Left(eValveStates::Hold);
-//  			Cio::is().Right(eValveStates::Dump);
-//  		}
-
- 		// pitch   roll       left  right
- 		// high     ok         dump   dump
-// 		else
-// 		{
-// 			Cio::is().Left(eValveStates::Dump);
-// 			Cio::is().Right(eValveStates::Dump);
-// 		}
-// 	}	
-// 	//Pitch Low
-// 	else if( pitch < Z - pitchtol )
-// 	{
-// 		// pitch   roll       left  right
-// 		// low      rightlow   hold   fill
-// 		if(roll > X + rolltol)
-// 		{
-// 			Cio::is().Left(eValveStates::Hold);
-// 			Cio::is().Right(eValveStates::Fill);
-// 		}
-// 		// pitch   roll       left  right
-// 		// low      leftlow    fill   hold
-// 		else if(roll < X - rolltol)
-// 		{
-// 			Cio::is().Left(eValveStates::Fill);
-// 			Cio::is().Right(eValveStates::Hold);
-// 		}
-// 		// pitch   roll       left  right
-// 		// low      ok         fill   fill
-// 		else
-// 		{
-// 			Cio::is().Left(eValveStates::Fill);
-// 			Cio::is().Right(eValveStates::Fill);
-// 		}
-// 	}
-// 	
-// 	else //pitch OK
-// 	{
-// 		// pitch   roll       left  right
-// 		// ok       ok         hold   hold
-// 		if(roll > X + rolltol)
-// 		{
-// 			Cio::is().Left(eValveStates::Hold);
-// 			Cio::is().Right(eValveStates::Hold);
-// 			
-// 			//wait a few seconds to make sure we are actually level
-// 			//leveling complete!!
-// 		}
-// 		// pitch   roll       left  right
-// 		// ok       leftlow    fill   dump
-// 		else if(roll < X - rolltol)
-// 		{
-// 			Cio::is().Left(eValveStates::Fill);
-// 			Cio::is().Right(eValveStates::Dump);
-// 		}
-// 		// pitch   roll       left  right
-// 		// ok       rightlow   dump   fill
-// 		else
-// 		{
-// 			Cio::is().Left(eValveStates::Dump);
-// 			Cio::is().Right(eValveStates::Fill);
-// 		}
-// 		// pitch   roll       left  right
-// 		// ok       lefthigh   dump   fill
-// 		else
-// 		{
-// 			Cio::is().Left(eValveStates::Dump);
-// 			Cio::is().Right(eValveStates::Fill);
-// 		}
-// 		// pitch   roll       left  right
-// 		// ok       righthigh  fill   dump
-// 		else
-// 		{
-// 			Cio::is().Left(eValveStates::Fill);
-// 			Cio::is().Right(eValveStates::Dump);
-// 		}
-//	}
-	
+   }
 }
 
