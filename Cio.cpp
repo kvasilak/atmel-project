3b20178683a7d4d12abe1bb7b2f617e9f69cf982
kvasilakes <my427v8@hotmail.com>
2016-04-06 22:14:25 -0500

    Hardware Rev 3

--- Cio.cpp
+++ Cio.cpp
@@ -19,6 +19,9 @@
 #include <avr/sleep.h>
 
 volatile bool Cio::IgnitionChanged = false;
+volatile uint8_t ispa;
+
+#define REV3_PCB
 
 
 // default constructor
@@ -34,30 +37,33 @@ DumpPressed(false)
 //Write using PORTx
 void Cio::Direction()
 {
+	#ifdef REV2_PCB
 		//port A
 		DDRA = 0x0C;
 		
-		//0		ADC		Left Height
-		//1		ADC		Right height
-		//2		Out		power up
-		//3		OUt		Compressor
-		//4		in		Remote RU
-		//5		in		Remote RD
-		//6		in		Remote LU
-		//7		in		Remote LD
+		//0		ADC		0	Left Height
+		//1		ADC		0	Right height
+		//2		Out		1	power up
+		//3		OUt		1	Compressor
+		
+		//4		in		0	Remote RU
+		//5		in		0	Remote RD
+		//6		in		0	Remote LU
+		//7		in		0	Remote LD
 		
 		
 		DDRB = 0xF0;
 		
 		//port B
-		//0		in		Remote up
-		//1		in		Remote Down
-		//2		in		Remote Travel
-		//3		in		Remote Camp
-		//4		Out		Right Up
-		//5		Out		Right Down
-		//6		Out		Left up
-		//7		Out		Left Down
+		//0		in		0	Remote up
+		//1		in		0	Remote Down
+		//2		in		0	Remote Travel
+		//3		in		0	Remote Camp
+		
+		//4		Out		1	Right Up
+		//5		Out		1	Right Down
+		//6		Out		1	Left up
+		//7		Out		1	Left Down
 		
 		//DDRC = 0x;
 		//Port C
@@ -72,42 +78,99 @@ void Cio::Direction()
 		
 		DDRD = 0x02;
 		//Port D
-		//0		RX		Debug serial
-		//1		TX		Debug serial
-		//2		in		Calibrate
-		//3		in		Ignition On
-		//4		in		switch Down
-		//5		in		switch Up
-		//6		in		switch Travel
-		//7		in		switch Camp
+		//0		RX		0	Debug serial
+		//1		TX		1	Debug serial
+		//2		in		0	Calibrate
+		//3		in		0	Ignition On
+		
+		//4		in		0	switch Down
+		//5		in		0	switch Up
+		//6		in		0	switch Travel
+		//7		in		0	switch Camp
+		#endif
+		
+		#ifdef REV3_PCB
+		//port A
+		DDRA = 0xF0;
+		
+		//0		ADC		0	Left Height
+		//1		ADC		0	Right height
+		//2		In		0	Ignition on
+		//3		ADC		0	Dimming
+		
+		//4		out		1	RD 
+		//5		out 	1	RU
+		//6		out		1	LD
+		//7		out		1	LU
+		
+		
+		DDRB = 0x00;
+		
+		//port B
+		//0		in		0	Remote LD
+		//1		in		0	Remote LU
+		//2		in		0	Remote RD
+		//3		in		0	Remote Camp
+		
+		//4		in 		0	Remote RU
+		//5		in 		0	Remote travel
+		//6		in 		0	Remote down
+		//7		in		0	Remote up
+		
+		DDRC = 0x80;
+		//Port C
+		//0		SCL
+		//1		SDA
+		//2		JTAG
+		//3		JTAG
+		//4		JTAG
+		//5		JTAG
+		//6		in		0 spare
+		//7		out		1 Compressor on
+		
+		DDRD = 0x02;
+		//Port D
+		//0		RX		0	Debug serial
+		//1		TX		1	Debug serial
+		//2		out		0	Power up
+		//3		in		0	Travel
+		
+		//4		in		0	Calibrate
+		//5		in		0	Camp
+		//6		in		0	Down
+		//7		in		0	up
+		#endif
 }
 
 void Cio::Pullups()
 {
+	#ifdef REV2_PCB
 		//port A
 		PORTA = 0xf0;
 		
-		//0		ADC		Left Height
-		//1		ADC		Right height
-		//2		Out		power up
-		//3		OUt		Compressor
-		//4		in		Remote RU
-		//5		in		Remote RD
-		//6		in		Remote LU
-		//7		in		Remote LD
+		//0		ADC		0	Left Height
+		//1		ADC		0	Right height
+		//2		Out		0	power up
+		//3		OUt		0	Compressor
+		
+		//4		in		1	Remote RU
+		//5		in		1	Remote RD
+		//6		in		1	Remote LU
+		//7		in		1	Remote LD
 		
 		
 		PORTB = 0x0F;
 		
 		//port B
-		//0		in		Remote up
-		//1		in		Remote Down
-		//2		in		Remote Travel
-		//3		in		Remote Camp
-		//4		Out		Right Up
-		//5		Out		Right Down
-		//6		Out		Left up
-		//7		Out		Left Down
+		//0		in		1	Remote up
+		//1		in		1	Remote Down
+		//2		in		1	Remote Travel
+		//3		in		1	Remote Camp
+		
+		//4		Out		0	Right Up
+		//5		Out		0	Right Down
+		//6		Out		0	Left up
+		//7		Out		0	Left Down
 		
 		//DDRC = 0x;
 		//Port C
@@ -122,14 +185,68 @@ void Cio::Pullups()
 		
 		PORTD = 0x00;
 		//Port D
-		//0		RX		Debug serial
-		//1		TX		Debug serial
-		//2		in		Calibrate
-		//3		in		Ignition On
-		//4		in		switch Down
-		//5		in		switch Up
-		//6		in		switch Travel
-		//7		in		switch Camp
+		//0		RX		0	Debug serial
+		//1		TX		0	Debug serial
+		//2		in		0	Calibrate
+		//3		in		0	Ignition On
+		
+		//4		in		0	switch Down
+		//5		in		0	switch Up
+		//6		in		0	switch Travel
+		//7		in		0	switch Camp
+		#endif
+		
+#ifdef REV3_PCB
+		//port A
+		PORTA = 0x00;
+		
+		//0		ADC		0	Left Height
+		//1		ADC		0	Right height
+		//2		In		0	Ignition on
+		//3		ADC		0	Dimming
+		
+		//4		out		0	RD
+		//5		out 	0	RU
+		//6		out		0	LD
+		//7		out		0	LU
+		
+		
+		PORTB = 0xFF;
+		
+		//port B
+		//0		in		1	Remote LD
+		//1		in		1	Remote LU
+		//2		in		1	Remote RD
+		//3		in		1	Remote Camp
+		
+		//4		in 		1	Remote RU
+		//5		in 		1	Remote travel
+		//6		in 		1	Remote down
+		//7		in		1	Remote up
+		
+		//PORTC = 0x00;
+		//Port C
+		//0		SCL
+		//1		SDA
+		//2		JTAG
+		//3		JTAG
+		//4		JTAG
+		//5		JTAG
+		//6		in		0 spare
+		//7		out		0 Compressor on
+		
+		PORTD = 0x00;
+		//Port D
+		//0		RX		0	Debug serial
+		//1		TX		0	Debug serial
+		//2		out		0	Power up
+		//3		in		0	Travel
+		
+		//4		in		0	Calibrate
+		//5		in		0	Camp
+		//6		in		0	Down
+		//7		in		0	up
+#endif
 }
 
 
@@ -140,11 +257,21 @@ void Cio::Init()
 	Pullups();
 	
 	//use to debounce switch inputs
-	PushCalibrate.Attach(IO_PORTD, PORTD2);
-	RockerDown.Attach(IO_PORTD, PORTD4);
-	RockerUp.Attach(IO_PORTD, PORTD5);
-	PushTravel.Attach(IO_PORTD, PORTD6);
-	PushCamp.Attach(IO_PORTD, PORTD7);
+//#ifdef REV2_PCB
+	//PushCalibrate.Attach(IO_PORTD, PORTD2);
+	//RockerDown.Attach(IO_PORTD, PORTD4);
+	//RockerUp.Attach(IO_PORTD, PORTD5);
+	//PushTravel.Attach(IO_PORTD, PORTD6);
+	//PushCamp.Attach(IO_PORTD, PORTD7);
+//#endif
+
+#ifdef REV3_PCB	
+	PushTravel.Attach(IO_PORTD, PORTD3);
+	PushCalibrate.Attach(IO_PORTD, PORTD4);
+	PushCamp.Attach(IO_PORTD, PORTD5);
+	RockerDown.Attach(IO_PORTD, PORTD6);
+	RockerUp.Attach(IO_PORTD, PORTD7);
+#endif
 	
 
 	//setup initial values
@@ -193,26 +320,26 @@ bool Cio::RockerChanged()
 
 bool Cio::OutSideRemoteChanged()
 {
-	static uint8_t OldPortA = false;
-	uint8_t porta = PINA & 0xF0;
+	//static uint8_t OldPort = false;
+	//uint8_t port = PINB & 0x71;
 	
-	bool changed = OldPortA != porta;
+	//bool changed = OldPort != port;
 	
-	OldPortA = porta;
+	//OldPort = port;
 	
-	return changed;
+	return false;//changed;
 }
 
 bool Cio::SteeringRemoteChanged()
 {
-	static uint8_t OldPortB = false;
-	uint8_t portb = PINB & 0x05;
+	//static uint8_t OldPort = false;
+	//uint8_t port = PINB & 0xE8;
 
-	bool changed = OldPortB != portb;
+	//bool changed = OldPort != port;
 
-	OldPortB = portb;
+	//OldPort = port;
 
-	return changed;
+	return false; //changed;
 }
 
 
@@ -222,18 +349,19 @@ bool Cio::SteeringRemoteChanged()
 //camp on steering remote
 bool Cio::CampChanged()
 {
-	static bool OldCamp					= false;
-	static bool OldInsideCamp			= false;
-	 
-	bool camp = (PINB & _BV(PORTB1));
- 	
-	bool Changed	= OldCamp			!= (bool)PushCamp;		//port D bit 7
-	Changed			|= OldInsideCamp	!= camp;  //port B bit 3
-	 
-	OldCamp		= (bool)PushCamp;
-	OldInsideCamp  = camp;
-
-	return Changed;
+	//static bool OldCamp					= false;
+	//static bool OldInsideCamp			= false;
+	 //
+	//bool camp = (PINB & _BV(PORTB3));
+ 	//
+	//bool Changed	= OldCamp			!= (bool)PushCamp;		//port D bit 7
+	//Changed			|= OldInsideCamp	!= camp;  //port B bit 3
+	 //
+	//OldCamp		= (bool)PushCamp;
+	//OldInsideCamp  = camp;
+//
+	//return Changed;
+	return false;
 }
 
 //returns true if the travel mode buttons have changed state
@@ -241,33 +369,37 @@ bool Cio::CampChanged()
 //camp on steering remote
 bool Cio::TravelChanged()
 {
-	static bool OldTravel				= false;
-	static bool OldInsideTravel			= false;	
+	//static bool OldTravel				= false;
+	//static bool OldInsideTravel			= false;	
+	//
+	//bool trav = (PINB & _BV(PORTB5));
+//
+	//bool Changed	= OldTravel				!= (bool)PushTravel;
+		 //Changed	|= OldInsideTravel		!= trav; 
+	//
+	//OldTravel		= (bool)PushTravel;
+	//OldInsideTravel = trav;
+	//
+	//return Changed;
 	
-	bool trav = (PINB & _BV(PORTB3));
-
-	bool Changed	= OldTravel				!= (bool)PushTravel;
-		 Changed	|= OldInsideTravel		!= trav; 
-	
-	OldTravel		= (bool)PushTravel;
-	OldInsideTravel = trav;
-	
-	return Changed;
+	return false;
 }
 
 bool Cio::CalibrateChanged()
 {
-	static bool OldCal	= false;
-	bool changed = false;
+	//static bool OldCal	= false;
+	//bool changed = false;
+	//
+	////only consider a press a change a release isn't a change
+	//if(PushCalibrate > 0)
+	//{
+		//changed = OldCal != (bool)PushCalibrate;
+	//}
+	//OldCal = (bool)PushCalibrate;
+	//
+	//return changed;
 	
-	//only consider a press a change a release isn't a change
-	if(PushCalibrate > 0)
-	{
-		changed = OldCal != (bool)PushCalibrate;
-	}
-	OldCal = (bool)PushCalibrate;
-	
-	return changed;
+	return false;
 }
 
 void Cio::RockerSwitch()
@@ -335,124 +467,124 @@ void Cio::RockerSwitch()
 
 void Cio::OutsideRemote()
 {
-	bool RemoteLeftUp = PINA & _BV(4);		//left down
-	bool RemoteLeftDown =PINA & _BV(6);		//right down
-	bool RemoteRightUp = PINA & _BV(5);		//left up
-	bool RemoteRightDown =PINA & _BV(7);	//right down
-	
-	FillPressed = false;
-	DumpPressed = false;
-	
-	if(RemoteLeftUp)
-		LeftFillOn();
-	else
-		LeftFillOff();
-		
-	if(RemoteRightUp)
-		RightFillOn();
-	else
-		RightFillOff();
-		
-	if(RemoteLeftDown)
-		LeftDumpOn();
-	else
-		LeftDumpOff();
-		
-	if(RemoteRightDown)
-		RightDumpOn();
-	else
-		RightDumpOff();
+	//bool RemoteLeftDown  = PINB & _BV(0);
+	//bool RemoteLeftUp    = PINB & _BV(1);		
+    //bool RemoteRightDown = PINB & _BV(2);
+	//bool RemoteRightUp   = PINB & _BV(4);		
+	//
+	//FillPressed = false;
+	//DumpPressed = false;
+	//
+	//if(RemoteLeftUp)
+		//LeftFillOn();
+	//else
+		//LeftFillOff();
+		//
+	//if(RemoteRightUp)
+		//RightFillOn();
+	//else
+		//RightFillOff();
+		//
+	//if(RemoteLeftDown)
+		//LeftDumpOn();
+	//else
+		//LeftDumpOff();
+		//
+	//if(RemoteRightDown)
+		//RightDumpOn();
+	//else
+		//RightDumpOff();
 	
 }
 
 void Cio::SteeringRemote()
 {
-	bool RemoteUp = PINB & _BV(2);
-	bool RemoteDown = PINB & _BV(0);
-	
-	if(RemoteUp)
-	{
-		if(FillPressed)
-		{
-			LeftFillOff();
-			RightFillOff();
-			
-			FillPressed = false;
-			
-		}
-		else
-		{
-			//turn off and unlatch the dump button
-			if(DumpPressed)
-			{
-				LeftDumpOff();
-				RightDumpOff();
-				
-				DumpPressed = false;
-			}
-			
-			LeftFillOn();
-			RightFillOn();
-			
-			FillPressed = true;
-			DumpPressed = false;
-		}
-		
-	}
+	//bool RemoteUp = PINB & _BV(7);
+	//bool RemoteDown = PINB & _BV(6);
+	//
+	//if(RemoteUp)
+	//{
+		//if(FillPressed)
+		//{
+			//LeftFillOff();
+			//RightFillOff();
+			//
+			//FillPressed = false;
+			//
+		//}
+		//else
+		//{
+			////turn off and unlatch the dump button
+			//if(DumpPressed)
+			//{
+				//LeftDumpOff();
+				//RightDumpOff();
+				//
+				//DumpPressed = false;
+			//}
+			//
+			//LeftFillOn();
+			//RightFillOn();
+			//
+			//FillPressed = true;
+			//DumpPressed = false;
+		//}
+		//
+	//}
 
 	
-	if(RemoteDown)
-	{
-		if(DumpPressed)
-		{
-			LeftDumpOff();
-			RightDumpOff();
-			
-			DumpPressed = false;
-			
-		}
-		else
-		{
-			//turn off and unlatch the fill button
-			if(FillPressed)
-			{
-				LeftFillOff();
-				RightFillOff();
-				
-				FillPressed = false;
-			}
-			
-			LeftDumpOn();
-			RightDumpOn();
-			
-			DumpPressed = true;
-		}
-	}
+	//if(RemoteDown)
+	//{
+		//if(DumpPressed)
+		//{
+			//LeftDumpOff();
+			//RightDumpOff();
+			//
+			//DumpPressed = false;
+			//
+		//}
+		//else
+		//{
+			////turn off and unlatch the fill button
+			//if(FillPressed)
+			//{
+				//LeftFillOff();
+				//RightFillOff();
+				//
+				//FillPressed = false;
+			//}
+			//
+			//LeftDumpOn();
+			//RightDumpOn();
+			//
+			//DumpPressed = true;
+		//}
+	//}
 
 }
 
 
 void Cio::CampSwitches()
 {
-	bool SteeringCamp = PINB & _BV(1);
-	
-	if(!PushCamp.Level() || SteeringCamp)
-	{
-		CLeds::is().CampOn();
-		CLeds::is().TravelOKOff();
-	}
+	//bool SteeringCamp = PIND & _BV(5);
+	//
+	//if(!PushCamp.Level() || SteeringCamp)
+	//{
+		//CLeds::is().CampOn();
+		//CLeds::is().TravelOKOff();
+	//}
 
 }
 
 void Cio::TravelSwitches()
 {
-	bool SteeringTravel = PINB & _BV(3);
-	
-	if(!PushTravel.Level() || SteeringTravel)
-	{
-		CLeds::is().CampOff();
-		CLeds::is().TravelOKOn();
-	}
+	//bool SteeringTravel = PIND & _BV(3);
+	//
+	//if(!PushTravel.Level() || SteeringTravel)
+	//{
+		//CLeds::is().CampOff();
+		//CLeds::is().TravelOKOn();
+	//}
 
 }
 
@@ -511,51 +643,51 @@ void Cio::Left(eValveStates s)
 
 void Cio::RightFillOn()
 {
-	PORTB |= _BV(4);
+	PORTA |= _BV(5);
 	CLeds::is().RightFillOn();
 }
 
 void Cio::RightFillOff()
 {
-	PORTB &= ~_BV(4);
+	PORTA &= ~_BV(5);
 	CLeds::is().RightFillOff();
 }
 
 void Cio::RightDumpOn()
 {
-	PORTB |= _BV(5);
+	PORTA |= _BV(4);
 	CLeds::is().RightDumpOn();
 }
 
 void Cio::RightDumpOff()
 {
-	PORTB &= ~_BV(5);
+	PORTA &= ~_BV(4);
 	CLeds::is().RightDumpOff();
 }
 
 
 void Cio::LeftFillOn()
 {
-	PORTB |= _BV(6);
+	PORTA |= _BV(7);
 	CLeds::is().LeftFillOn();
 }
 
 void Cio::LeftFillOff()
 {
-	PORTB &= ~_BV(6);
+	PORTA &= ~_BV(7);
 	CLeds::is().LeftFillOff();
 }
 
 void Cio::LeftDumpOn()
 {
-	PORTB |= _BV(7);
+	PORTA |= _BV(6);
 	//PORTA |= _BV(3);
 	CLeds::is().LeftDumpOn();
 }
 
 void Cio::LeftDumpOff()
 {
-	PORTB &= ~_BV(7);
+	PORTA &= ~_BV(6);
 	CLeds::is().LeftDumpOff();
 }
 
@@ -575,24 +707,24 @@ bool Cio::IsHolding()
 void Cio::PowerOn()
 {
 	//Turn on 5v, PA2
-	PORTA |= _BV(PA2);
+	PORTD |= _BV(PD2);
 }
 
 void Cio::PowerOff()
 {
 	//Turn off 5v, PA2
-	PORTA &= ~_BV(PA2);
+	PORTD &= ~_BV(PD2);
 }
 
 void Cio::CompressorOn()
 {
 	//Turn on Air compressor enable, PA3
-	PORTA |= _BV(PA3);
+	PORTC |= _BV(PC7);
 }
 
 void Cio::CompressorOff()
 {
-	PORTA &= ~_BV(PA3);
+	PORTC &= ~_BV(PC7);
 }
 
 //put the system into sleep to conserve power
@@ -650,7 +782,7 @@ void Cio::Wakeup()
 
  bool Cio::IsIgnitionOn()
  {
- 	return (PIND & _BV(3)) & _BV(3);
+ 	return (PINA & _BV(2)) & _BV(2);
  }
 
 // clock interrupt - clear flag immediately to resume count
